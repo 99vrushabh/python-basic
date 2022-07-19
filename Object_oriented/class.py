80c21f20e5e7747a2ee5b6c6aa429b9a97a326a4
vrushabh <vrushabh160.rejoice@gmail.com>
2022-07-19 15:15:28 +0530

    initial changes

---
+++ Object_oriented/class.py
@@ -0,0 +1,13 @@
+from email.mime import application
+
+
+class form:
+    formType ="SubmittionForm"
+    def printData(self):
+        print(f"name is {self.name}")
+        print(f"train is {self.train}")
+
+formApplication = form()
+formApplication.name = "hello22"
+formApplication.train = "abc008902 express"
+formApplication.printData()
