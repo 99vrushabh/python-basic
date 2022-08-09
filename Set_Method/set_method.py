7fd0b19b9b08f99141f115f1cc3326858e117a46
vrushabh <vrushabh160.rejoice@gmail.com>
2022-08-09 17:48:23 +0530

    initial changes

--- Set_Method/set_method.py
+++ Set_Method/set_method.py
@@ -1,3 +1,8 @@
+from calendar import c
+from os import scandir
+from re import A
+
+
 s1={1,2,3,4,5,6}
 s2={4,5}
 s3={8,9}
@@ -7,13 +12,13 @@ s3={8,9}
 print("s1 is :",s1,"\ns2 is :",s2,"\ns3 is :",s3)
 # this is math method of intersection
 # it means in dict a and dict b have same elements
-d=(s1.intersection(s2))
-print("s1 intersectin of s2 is : ",d)
+s4=(s1.intersection(s2))
+print("s1 intersectin of s2 is : ",s4)
 
 # this is math method of union
 # it means in dict a and dict b have all the  elements in new dict
-d=(s1.union(s2))
-print("s1 Union s2 is : ",d)
+s5=(s1.union(s2))
+print("s1 Union s2 is : ",s5)
 
 # update exist dict 
 s1.update([10,11])
@@ -25,20 +30,35 @@ s1.remove(3)
 print("after element(3) remove , new dict s1 is",s1)
 
 # for diffrence betwen two dict
-s4=s1.difference(s2)
-print ("diffrence between s1 and s2",s4)
+s5 =s1.difference(s2)
+print ("diffrence between s1 and s2",s5 )
 print("diffrence between s1 and s2",s1.symmetric_difference(s2))
 
-s5=s1.intersection(s2).union(s3)
-print(s5)
+s6=s1.intersection(s2).union(s3)
+print(s6)
 
 
 # math method like subset or superset
-s6=s2.issubset(s1)
-print(s6)
-s7=s1.issuperset(s2)
+s7=s2.issubset(s1)
 print(s7)
+s8=s1.issuperset(s2)
+print(s8)
 
+# for get avg of two numbers
+a=int(input("enter a : "))
+b=int(input("enter b : "))
+c=(a+b)/2
+
+print("a is :",a)
+print("b is :",b)
+print("c is :",c)
+
+def findItem(e, l):
+    for i, el in enumerate(l):
+        if el == e:
+            return [i, el]
+    
+    return findItem
 # It is possible to print a generator as a list, but you will loose performance
 # In a large set of data generators excel in time efficiency
 # print(list(myGen))
